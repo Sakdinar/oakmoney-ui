a4284170f26ea685f24eecac20e47e4bf358a175
Anderson Carvalho <anderson.cvlh@gmail.com>
2017-08-25 07:58:56 -0300

    Adicionando componente calendario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { ButtonModule } from 'primeng/components/button/button';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
-import {TooltipModule} from 'primeng/components/tooltip/tooltip';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
 import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
 
 import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
@@ -28,7 +30,9 @@ import { LancamentosCadastroComponent } from './lancamentos-cadastro/lancamentos
     ButtonModule,
     DataTableModule,
     TooltipModule,
-    InputTextareaModule
+    InputTextareaModule,
+    CalendarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
