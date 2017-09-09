8e28a098b4c063e405188a79b814e542c33d47a3
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-09-09 14:52:48 -0300

    Remodelando formulário baseados nas classes de modelo.

--- src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
+++ src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
@@ -1,4 +1,10 @@
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+import { ErrorHandlerService } from './../../oak-core/error-handler.service';
+import { CategoriasService } from './../../categorias/categorias.service';
+import { PessoasService } from './../../pessoas/pessoas.service';
+import { Lancamento } from './../../oak-core/models/lancamento';
 
 @Component({
   selector: 'app-lancamentos-cadastro',
@@ -7,23 +13,42 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentosCadastroComponent implements OnInit {
 
   tipos = [
-    { label: 'Receita', value: 0},
-    { label: 'Despesa', value: 1}
+    { label: 'Receita', value: 'RECEITA'},
+    { label: 'Despesa', value: 'DESPESA'}
   ];
 
-  categorias = [
-    { label: 'Alimentação', value: 1},
-    { label: 'Transporte', value: 2}
-  ];
+  categorias = [];
+  pessoas = [];
+  lancamento = new Lancamento();
 
-  pessoas = [
-    { label: 'Anderson', value: 1},
-    { label: 'Allison', value: 2}
-  ];
-
-  constructor() { }
+  constructor(
+    private categoriasService: CategoriasService,
+    private pessoasService: PessoasService,
+    private errorHandler: ErrorHandlerService
+  ) { }
 
   ngOnInit() {
+    this.carregarCategorias();
+    this.carregarPessoas();
+  }
+
+  private carregarCategorias() {
+    return this.categoriasService.listarTodas()
+      .then(categorias => {
+        this.categorias = categorias.map(c =>  ({ label: c.nome, value: c.codigo }) );
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  salvar(form: FormControl) {
+    console.log(this.lancamento);
+  }
+  private carregarPessoas() {
+    return this.pessoasService.listarTodas()
+      .then(pessoas => {
+        this.pessoas = pessoas.map(p => ( {label: p.nome, value: p.codigo} ));
+      })
+      .catch((erro) => this.errorHandler.handle(erro));
   }
 
 }
