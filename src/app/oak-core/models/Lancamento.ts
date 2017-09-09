58cfb1a5bbc9d787f396e5cdd95a24babdd14a89
Anderson Carvalho Martins <anderson.cvlh@gmail.com>
2017-09-09 14:51:18 -0300

    Iniciando as classes modelo

---
+++ src/app/oak-core/models/Lancamento.ts
@@ -0,0 +1,15 @@
+import { Categoria } from './categoria';
+import { Pessoa } from './pessoa';
+
+export class Lancamento {
+  codigo: number;
+  tipo = 'RECEITA';
+  descricao: string;
+  dataVencimento: Date;
+  dataPagamento: Date;
+  valor: number;
+  observacao: string;
+  pessoa = new Pessoa();
+  categoria = new Categoria();
+}
+
